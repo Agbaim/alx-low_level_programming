2149039b34065938a7fd94234325097f84b4f359
Agbaim <agbaimd@yahoo.com>
2022-09-08 06:55:43 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,11 @@
+task4 
 #include <stdio.h>
-
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ *  * main - main block
+ *   * Return: 0
+ *    */
 int main(void)
-
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
-
 }
