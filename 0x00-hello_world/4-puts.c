bb03c073473e3839fb8f4bfa4e9991e8e0013441
Agbaim <agbaimd@yahoo.com>
2022-09-08 07:06:59 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,18 @@
-task4 
 #include <stdio.h>
+
 /**
- *  * main - main block
- *   * Return: 0
- *    */
+ *
+ *  * main - entry point
+ *
+ *   *
+ *
+ *    * Return: 0 after printing the function
+ *
+ *     */
+
 int main(void)
-{
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+
+{puts(*\ *Programming is like building a multilingual puzzle*);
+
+	 return (0);
 }
