c77a0d32470ffc3b99fdd0d9508c342848c7bf68
Agbaim <agbaimd@yahoo.com>
2022-09-13 05:44:57 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+/**
+ * main - program startup
+(*
+ * Description: prints `_putchar`)?
+ * Return: return (0) is the required function signature
+ */
+
+#include "main.h"
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
+
