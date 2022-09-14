b73c581962ddcb1f0d669eff304f7ce799725f59
Agbaim <agbaimd@yahoo.com>
2022-09-13 19:05:47 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,52 +1,46 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * print_to_98 - prints all natural numbers from n to 98
- * @n: input integer from which the count starts until 98
- *
- * Return: 0 on success
+ * print_to_98- print_alphabet
+ * @n: variable
+ * Return: 0 or 1
  */
 void print_to_98(int n)
 {
-	int delta = 1;
-	int current = n;
-
-	if (n > 98)
-		delta = -1;
-	/*print current n*/
-	if (current < 0)
-		_putchar('-');
-	print_any_int(n);
-
-	while (current != 98)
-	{
-		_putchar(',');
-		_putchar(' ');
-		current = current + delta;
-		if (current < 0)
-			_putchar('-');
-		print_any_int(current);
-	}
-	_putchar('\n');
+if (n <= 98)
+{
+while (n <= 98)
+{
+printf("%d", n);
+n++;
+if (n < 99)
+{
+printf(", ");
+}
+else
+{
+printf("\n");
+}
+}
 }
 
-/**
- * print_any_int - uses _putchar to print every digit of any int
- * @m: input integer to be printed with _putchar
- *
- * Return: void, printing every digit of m into stdout
- */
-void print_any_int(int m)
+else if (n >= 98)
 {
-	int last;
-	if (m / 10)
-		print_any_int(m / 10);
+while (n >= 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+else
+{
+printf("\n");
+}
+n--;
 
-	last = m % 10;
-	if (last < 0)
-		last = last * -1;
-	_putchar(last + '0');
-}	
+}
+}
 
-	
+}
 
